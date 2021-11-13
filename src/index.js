9b230671cfa013cd23bc65cdf4c141a79a357a44
Kevin <oiaeuvoando@gmail.com>
2021-11-12 22:02:22 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -201,7 +201,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage: storage});
+const upload = multer({storage: storage}); 
 
 app.post('/produto/:id/:id2',upload.single('imgPrincipal') , async (req, resp) => {
     try {
